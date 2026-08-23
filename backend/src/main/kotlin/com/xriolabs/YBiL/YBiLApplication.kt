5fb1e67b55588f1a66d755e8d6586346f0e91f33
Neminda Bandara <nemindabandara15@gmail.com>
2026-08-23 13:59:34 +0545

    backend initiated

---
+++ backend/src/main/kotlin/com/xriolabs/YBiL/YBiLApplication.kt
@@ -0,0 +1,11 @@
+package com.xriolabs.YBiL
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YBiLApplication
+
+fun main(args: Array<String>) {
+	runApplication<YBiLApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/xriolabs/YBiL/YBiLApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xriolabs.YBiL
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YBiLApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
